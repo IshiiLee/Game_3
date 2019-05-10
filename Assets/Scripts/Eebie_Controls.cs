3eadd23991492a9bda95dc8ebbef66380b49a44e
IshiiLee <49171713+IshiiLee@users.noreply.github.com>
2019-05-10 16:32:09 -0600

    Updates

--- Assets/Scripts/Eebie_Controls.cs
+++ Assets/Scripts/Eebie_Controls.cs
@@ -9,10 +9,18 @@ public class Eebie_Controls : MonoBehaviour
 
     Animator anim;
 
+    bool grounded = false;
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 20f;
+    bool doubleJump = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
 
 
     }
@@ -20,8 +28,14 @@ public class Eebie_Controls : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+
+        if (grounded)
+            doubleJump = false; 
+
+        anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+
         float move = Input.GetAxis("Horizontal");
 
         anim.SetFloat("Speed", Mathf.Abs(move));
@@ -32,6 +46,17 @@ public class Eebie_Controls : MonoBehaviour
             Flip();
         else if (move < 0 && facingRight)
             Flip();
+    }
+
+    void Update()
+    {
+        if ((grounded || !doubleJump) && Input.GetKeyDown(KeyCode.UpArrow))
+            anim.SetBool("Ground", false);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+
+        if (!doubleJump && !grounded)
+            doubleJump = true;
+
     }
 
     void Flip()
